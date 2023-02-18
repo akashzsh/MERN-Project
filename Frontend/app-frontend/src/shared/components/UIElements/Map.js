5e5bee82c7cf6e083082ad93d06394be52c3e0a7
Akash <akashudsouza12@gmail.com>
2023-02-18 15:36:28 +0530

    google map paid content cancelled

---
+++ Frontend/app-frontend/src/shared/components/UIElements/Map.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Map.css";
+
+const Map = (props) => {
+  return (
+    <div className={`map ${props.className}`} style={`${props.style}`}>
+      <h2>MAP PAID CONTENT</h2>
+    </div>
+  );
+};
+
+export default Map;
