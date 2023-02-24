74f559a35f1f297e350f92d10a8e55a67e3a232d
Akash <akashudsouza12@gmail.com>
2023-02-24 18:19:41 +0530

    input forms

--- Frontend/app-frontend/src/places/pages/NewPlace.js
+++ Frontend/app-frontend/src/places/pages/NewPlace.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
 import "./NewPlace.css";
 
 export default function NewPlace() {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
         errorText="Please enter a valid title"
         validators={[VALIDATOR_REQUIRE()]}
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        type="text"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (atleast 5 characters)"
+        onInput={descriptionInputHandler}
       />
     </form>
   );
