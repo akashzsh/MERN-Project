aa3b5fa7a8d06806adf1737d45f34471375cb5c6
Akash <akashudsouza12@gmail.com>
2023-03-02 20:36:51 +0530

    Auth style and css modification

--- Frontend/app-frontend/src/users/pages/Auth.js
+++ Frontend/app-frontend/src/users/pages/Auth.js
@@ -7,9 +7,11 @@ import {
 } from "../../shared/util/validators";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
+import Card from "../../shared/components/UIElements/Card";
+import "./Auth.css";
 
 const Auth = () => {
-  const [formState, inputHandler, setFormData] = useForm(
+  const [formState, inputHandler] = useForm(
     {
       email: {
         value: "",
@@ -23,33 +25,39 @@ const Auth = () => {
     false
   );
 
-  const placeSubmitHandler = () => {
-    console.log("SUBMIT");
+  const placeSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
   };
 
   return (
-    <form className="place-form" onSubmit={placeSubmitHandler}>
-      <Input
-        id="email"
-        element="input"
-        label="Email"
-        validators={[VALIDATOR_EMAIL()]}
-        errorText="Please enter a valid email"
-        onInput={inputHandler}
-      />
-      <Input
-        id="password"
-        element="input"
-        label="Password"
-        type="password"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid password"
-        onInput={inputHandler}
-      />
-      <Button type="submit" disabled={!formState.isValid}>
-        ADD PLACE
-      </Button>
-    </form>
+    <Card className="authentication">
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={placeSubmitHandler}>
+        <Input
+          id="email"
+          element="input"
+          label="Email"
+          type="email"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email"
+          onInput={inputHandler}
+        />
+        <Input
+          id="password"
+          element="input"
+          label="Password"
+          type="password"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid password (atleast 5 characters)"
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          LOGIN
+        </Button>
+      </form>
+    </Card>
   );
 };
 
