7f69537bf089ec6d69a686462cbdce94cd1bbb16
Akash <akashudsouza12@gmail.com>
2023-03-03 15:28:19 +0530

    auth context for listening to changes

--- Frontend/app-frontend/src/shared/components/Navigation/NavLinks.js
+++ Frontend/app-frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../context/auth-context";
 import "./NavLinks.css";
 
 const NavLinks = () => {
+  const auth = useContext(AuthContext);
+
   return (
     <ul className="nav-links">
       <li>
@@ -11,15 +14,21 @@ const NavLinks = () => {
           ALL USERS
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/u1/places">MY PLACES</NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/new">ADD PLACES</NavLink>
-      </li>
-      <li>
-        <NavLink to="/login">LOGIN</NavLink>
-      </li>
+      {auth.isLoggedIn && (
+        <React.Fragment>
+          <li>
+            <NavLink to="/u1/places">MY PLACES</NavLink>
+          </li>
+          <li>
+            <NavLink to="/places/new">ADD PLACES</NavLink>
+          </li>
+        </React.Fragment>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/login">LOGIN</NavLink>
+        </li>
+      )}
     </ul>
   );
 };
