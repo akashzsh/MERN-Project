05828bb77a39d1ddb032fb8f7aa02b292d60d7b3
Akash <akashudsouza12@gmail.com>
2023-03-03 15:24:01 +0530

    context for auth process

--- Frontend/app-frontend/src/App.js
+++ Frontend/app-frontend/src/App.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Outlet } from "react-router-dom";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <div>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        login: login,
+        logout: logout,
+      }}
+    >
       <div>
-        <MainNavigation />
-      </div>
-      <main>
         <div>
-          <Outlet />
+          <MainNavigation />
         </div>
-      </main>
-    </div>
+        <main>
+          <div>
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
