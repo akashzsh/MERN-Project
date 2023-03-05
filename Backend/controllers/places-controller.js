43280e16379e3ee0ce046d018d3b1a1634853a67
Akash <akashudsouza12@gmail.com>
2023-03-05 12:02:27 +0530

    added controllers for request logic

---
+++ Backend/controllers/places-controller.js
@@ -0,0 +1,55 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Australia",
+    description:
+      "Australia is a vast and diverse continent with unique flora and fauna, stunning natural landscapes, and a rich cultural heritage.",
+    location: {
+      lat: -21.0160213,
+      lng: 95.1370107,
+    },
+    address: "Next to New Zealand",
+    creatorId: "u1",
+  },
+  {
+    id: "p2",
+    title: "South Africa",
+    description:
+      "South Africa is a vast and diverse continent with unique flora and fauna, stunning natural landscapes, and a rich cultural heritage.",
+    location: {
+      lat: -33.2359591,
+      lng: 9.0968524,
+    },
+    address: "Next to England",
+    creatorId: "u2",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const place = DUMMY_PLACES.find((currPlace) => {
+    return currPlace.id === req.params.pid;
+  });
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place with the provided id", 404)
+    );
+  }
+  res.json({ message: place });
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const creator = DUMMY_PLACES.find((currPlace) => {
+    return currPlace.creatorId === req.params.uid;
+  });
+  if (!creator) {
+    return next(
+      new HttpError("Could not find a place with the provided user id", 404)
+    );
+  }
+  res.json({ message: creator });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
--- Backend/routes/places-routes.js
+++ Backend/routes/places-routes.js
@@ -1,57 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const HttpError = require("../models/http-error");
+const placesController = require("../controllers/places-controller");
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Australia",
-    description:
-      "Australia is a vast and diverse continent with unique flora and fauna, stunning natural landscapes, and a rich cultural heritage.",
-    location: {
-      lat: -21.0160213,
-      lng: 95.1370107,
-    },
-    address: "Next to New Zealand",
-    creatorId: "u1",
-  },
-  {
-    id: "p2",
-    title: "South Africa",
-    description:
-      "South Africa is a vast and diverse continent with unique flora and fauna, stunning natural landscapes, and a rich cultural heritage.",
-    location: {
-      lat: -33.2359591,
-      lng: 9.0968524,
-    },
-    address: "Next to England",
-    creatorId: "u2",
-  },
-];
-
-router.get("/:pid", (req, res, next) => {
-  const place = DUMMY_PLACES.find((currPlace) => {
-    return currPlace.id === req.params.pid;
-  });
-  if (!place) {
-    return next(
-      new HttpError("Could not find a place with the provided id", 404)
-    );
-  }
-  res.json({ message: place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const creator = DUMMY_PLACES.find((currPlace) => {
-    return currPlace.creatorId === req.params.uid;
-  });
-  if (!creator) {
-    return next(
-      new HttpError("Could not find a place with the provided user id", 404)
-    );
-  }
-  res.json({ message: creator });
-});
+router.get("/:pid", placesController.getPlaceById);
+router.get("/user/:uid", placesController.getPlaceByUserId);
 
 module.exports = router;
