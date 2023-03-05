ef8fb94517a810dc215a2e6ec74092e236c25796
Akash <akashudsouza12@gmail.com>
2023-03-05 12:32:14 +0530

    PATCH request to update place

--- Backend/controllers/places-controller.js
+++ Backend/controllers/places-controller.js
@@ -57,6 +57,28 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ DUMMY_PLACES });
 };
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = {
+    ...DUMMY_PLACES.find((currPlace) => currPlace.id === placeId),
+  };
+
+  const placeIndex = DUMMY_PLACES.findIndex(
+    (currPlace) => currPlace.id == placeId
+  );
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+  res.status(200).json(updatedPlace);
+};
+
+const deletePlace = (req, res, next) => {};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
--- Backend/routes/places-routes.js
+++ Backend/routes/places-routes.js
@@ -6,5 +6,7 @@ const placesController = require("../controllers/places-controller");
 router.get("/:pid", placesController.getPlaceById);
 router.get("/user/:uid", placesController.getPlaceByUserId);
 router.post("/", placesController.createPlace);
+router.patch("/:pid", placesController.updatePlace);
+router.delete("/:pid", placesController.deletePlace);
 
 module.exports = router;
