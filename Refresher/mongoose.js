0c309c02d1bf3fe600b262b967eac2da88db0363
Akash <akashudsouza12@gmail.com>
2023-03-06 23:26:03 +0530

    migrating from mongodb driver syntax to mongoose

--- Refresher/mongoose.js
+++ Refresher/mongoose.js
@@ -1,11 +1,26 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 const ProductModel = require("./models/product");
+
+mongoose
+  .connect(
+    `mongodb+srv://akashzsh:${process.env.MONGODBPASSWORD}@mern.5gjhp6e.mongodb.net/products_test?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Connected to the DB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const createProduct = async (req, res, next) => {
   const createdProduct = new ProductModel({
     name: req.body.name,
     price: req.body.price,
   });
+  const result = await createdProduct.save();
+
+  res.json({ result });
 };
 
 exports.createProduct = createProduct;
