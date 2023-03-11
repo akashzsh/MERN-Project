00fc1401ccd4a1195e37ba65a8e8785633ef3ded
Akash <akashudsouza12@gmail.com>
2023-03-11 16:57:20 +0530

    GET users from custom hook

--- Frontend/app-frontend/src/users/pages/Users.js
+++ Frontend/app-frontend/src/users/pages/Users.js
@@ -1,38 +1,31 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { UsersList } from "../components/UsersList";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 export default function Users() {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
+  const { loading, error, sendRequest, clearError } = useHttpClient();
   const [loadedUsers, setLoadedUsers] = useState();
 
   const getUsers = async () => {
-    setLoading(true);
     try {
-      const response = await axios.get("http://localhost:5000/api/users");
-      setLoadedUsers(response.data.users);
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      setError(error.response.data.message || "Unknown Error Occurred");
-      console.log(error.response.data.message || "Unknown Error Occurred");
-    }
-  };
-
-  const errorHandler = () => {
-    setError(null);
+      const response = await sendRequest(
+        "http://localhost:5000/api/users",
+        "get"
+      );
+      console.log(response.users);
+      setLoadedUsers(response.users);
+    } catch (error) {}
   };
 
   useEffect(() => {
     getUsers();
-  }, []);
+  }, [sendRequest]);
 
   return (
     <React.Fragment>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {loading && (
         <div className="center">
           <LoadingSpinner />
