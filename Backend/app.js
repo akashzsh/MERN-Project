7b769e5b2c22a4d681210e8721d3a6a77255f623
Akash <akashudsouza12@gmail.com>
2023-03-14 21:56:49 +0530

    image handling api route

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
 require("dotenv").config();
 
 const placesRoutes = require("./routes/places-routes");
@@ -10,6 +12,8 @@ const HttpError = require("./models/http-error");
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -29,6 +33,9 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => console.log(err));
+  }
   if (res.headerSent) return next(error);
   res
     .status(error.code || 500)
